deefafc599fdc712d45ed2f97fd5c5f0df65b064
Mehboob Chhipa <mehboob@chhipa.com>
2018-07-02 20:30:10 +0300

    added form for the chat

--- public/js/index.js
+++ public/js/index.js
@@ -4,16 +4,23 @@ socket.on('connect',()=>{
 
    socket.on("newMessage", (message)=>{
        console.log(message);
+       var li = jQuery('<li></li>');
+       li.text(`${message.from}: ${message.text}`);
+       jQuery('#messages').append(li);
    })
-    socket.emit('createMessage',{
-        from:"anonymous",
-        text:"doing just fine",
-        createdAt: new Date().getTime().toString()
-    });
+    // socket.emit('createMessage',{
+    //     from:"anonymous",
+    //     text:"doing just fine",
+    //     createdAt: new Date().getTime().toString()
+    // });
 
+    
+});
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
     socket.emit('createMessage', {
         from:"Alex",
-        text:'message'
+        text: jQuery('[name=message]').val()
         },  (st) => {
         console.log(st);
         });
